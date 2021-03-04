8eb14762e9aa686f862693eb58729c5d58a67376
DAWyolanda <646618@alu.murciaeduca.es>
2021-03-04 17:50:14 +0100

    Segunda versión de prueba

--- GitHub2/src/HolaMundo.java
+++ GitHub2/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola Mundo de GitHub!");
+		System.out.print("Esto es una práctica de Repositorio de GitHub");
 
 	}
 
